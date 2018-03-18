88f9c1abb052145b5bb614e88a597bd9ca6c589a
Anush S Kumar <anushkumar27@gmail.com>
2018-03-18 12:14:45 +0530

    Initial implmentation
    Co-authored-by: Anush S Kumar <anushkumar27@gmail.com>
    Co-authored-by: Sushrith Arkal <sushrith.arkal@gmail.com>

---
+++ src/planner.cpp
@@ -0,0 +1,43 @@
+/** Authors
+ * Anush S Kumar (anushkumar27@gmail.com)
+ * Sushrith Arkal (sushrith.arkal@gmail.com)
+**/
+
+#include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <geometry_msgs/PoseStamped.h>
+
+// CB for RPLidar data
+std_msgs::Float32MultiArray oa_data;
+void oa_data_cb(const std_msgs::Float32MultiArray::ConstPtr& msg) {
+	oa_data = *msg;
+}
+
+int main(int argc, char **argv) {   
+	// Initialise ros node and advertise to roscore
+	ros::init(argc, argv, "planner");
+
+	// NodeHandle is the main access point to communications with the ROS system.
+	ros::NodeHandle nh;
+
+	// SUBSCRIBER
+	// To fetch the current state of the FCU
+	ros::Subscriber oa_data_sub = nh.subscribe<std_msgs::Float32MultiArray>
+			("oa/data", 10, oa_data_cb);
+
+	// PUBLISHER
+	// To publish the raw destination pose to the FCU
+	ros::Publisher pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+			("auto_nav/mavros_bridge/pose", 10);
+
+	// The setpoint publishing rate MUST be faster than 2Hz
+	ros::Rate rate(20.0);
+
+	while(ros::ok()) {
+		// TODO
+		ros::spinOnce();
+		rate.sleep();
+	}
+
+	return 0;
+}
