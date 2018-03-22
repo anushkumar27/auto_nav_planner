e4fb94eb27cb647aea52bc272408f2409aad1a14
anushkumar27 <anushkumar27@gmail.com>
2018-03-22 14:20:16 +0530

    Added the commander subscriber
    Co-authored-by: Sushrith Arkal <sushrith.arkal@gmail.com>
    Co-authored-by: Anush S Kumar <anushkumar27@gmail.com>

--- src/planner.cpp
+++ src/planner.cpp
@@ -13,6 +13,12 @@ void oa_data_cb(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 	oa_data = *msg;
 }
 
+// CB for Commander dest pose
+geometry_msgs::PoseStamped dest_pose;
+void dest_pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    dest_pose = *msg;
+}
+
 int main(int argc, char **argv) {   
 	// Initialise ros node and advertise to roscore
 	ros::init(argc, argv, "planner");
@@ -24,6 +30,9 @@ int main(int argc, char **argv) {
 	// To fetch the current state of the FCU
 	ros::Subscriber oa_data_sub = nh.subscribe<std_msgs::Float32MultiArray>
 			("oa/data", 10, oa_data_cb);
+	// To fetch the dest from the commander node
+	ros::Subscriber dest_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+	("auto_nav/planner/dest/pose", 10, dest_pose_cb);
 
 	// PUBLISHER
 	// To publish the raw destination pose to the FCU
@@ -41,3 +50,4 @@ int main(int argc, char **argv) {
 
 	return 0;
 }
+
